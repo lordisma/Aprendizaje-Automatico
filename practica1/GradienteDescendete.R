7301b679b0900fef3fbe7798eb98807f6ecc91e4
Unknown <ismitae.imm@gmail.com>
2018-03-09 15:17:32 +0100

    Gradiente aun por arreglar

--- practica1/GradienteDescendete.R
+++ practica1/GradienteDescendete.R
@@ -1,5 +1,26 @@
-gradiente <-function(theta, w, x, y)
-{
-  w = w - theta * (2/length(x) * (x * ( det(t(w)%*%x) - y )) )
+library(ISLR)
+
+gradiente <- function( x, y, iteration){
   
+  w = rnorm(length(x[1,]) + 1)
+  x = cbind(x, rep(1,length(x[,1])))
+
+  
+  for (i in 1:100) {
+    print(w)
+    h = w%*%t(x)
+  
+    err = sum(abs(h - y)) / length(h)
+    h = h-y
+    theta = (-1) *( err / max(x)) #Mejorable
+    
+    w = w - theta * (2 / length(x[,1])) * (t(x)%*%t(h))
+    w = t(w)
+  }
+  
+  w
 }
+
+
+mm = matrix(1:200, nrow = 10, byrow = T)
+res = rep(1,length(mm[,1]))
