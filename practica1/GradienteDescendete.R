809d81f466a6bbb96850b7221b14a4d2d66fdebd
Unknown <ismitae.imm@gmail.com>
2018-03-16 04:13:36 +0100

    Nuevo version

--- practica1/GradienteDescendete.R
+++ practica1/GradienteDescendete.R
@@ -20,8 +20,32 @@ gradiente <- function(u, v, theta, epsilon){
   
 }
 
-w = gradiente(1,1,0.1, 10 * 10^-14)
+gradiente2 <- function(x, y, theta, iteration){
+  expr=expression( (x-2)^2 + 2*(y+2) + 2*sin(2*pi*x) * sin(2*pi*y) )
+  expr.Dx = D(expr, "x")
+  expr.Dy = D(expr, "y")
+  
+  err = abs(eval(expr))
+  it = 1
+  
+  result = data.frame(x,y,err,it)
+  
+  for(i in 1:iteration){
+    # Mirar como cambiarlo para varias caracteristicas
+    x = x - theta * eval(expr.Dx)
+    y = y - theta * eval(expr.Dy)
+    err = abs(eval(expr))
+    it = it +1  #Cuenta iteraciones
+    
+    result$x=append(result$x , x)
+    result$y=append(result$y , y)
+    result$err=append(result$err , err)
+    result$it=append(result$it , it)
+  }
+  
+  result
+  
+}
 
-expr=expression((  (u^3 * exp(v-2)) - (4 * v^3 * exp(-u))  )^2)
-
+w = gradiente2(1,1,0.01, 10)
 
